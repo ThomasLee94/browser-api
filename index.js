0ee0981cc5c50e1eaef6ce177c1e6ac7a9aa4c3c
ThomasLee94 <thomaslee9474@gmail.com>
2018-12-13 00:07:52 -0800

    [update] index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+const diagonalMove = [
+    {
+        transform: 'translate(0px, 0px)',
+        background: '#2196F3'
+    },
+    {
+        offset: .5,
+        transform: 'translate(115px, 115px)',
+        background: '#2196F3'
+    },
+    {
+        transform: 'translate(230px, 230px)',
+        background: '#EF5350'
+    }
+];
+
+let circle = document.getElementById('circle');
+
+circle.addEventListener('click', function() {
+    circle.animate(diagonalMove, {
+        duration: 400,
+        fill: 'forwards'
+    });
+});
+
+document.getElementById('pause').addEventListener('click', function() {
+  squareAnimation.pause();
+  document.getElementById('square').style.backgroundColor = "#EF5350";
+});
+document.getElementById('play').addEventListener('click', function() {
+  squareAnimation.play();
+  document.getElementById('square').style.backgroundColor = "#2196F3";
+});
+document.getElementById('reverse').addEventListener('click', function() {
+  squareAnimation.reverse();
+});
+
+document.getElementById('slow').addEventListener('click', function() {
+  squareAnimation.playbackRate = 0.5;
+});
+document.getElementById('normal').addEventListener('click', function() {
+  squareAnimation.playbackRate = 1;
+});
+document.getElementById('fast').addEventListener('click', function() {
+  squareAnimation.playbackRate = 2;
+});
+
+const square = document.getElementById('square');
+const squareAnimation = square.animate([
+    {
+        transform: 'rotate(0)'
+    },
+    {
+        transform: 'rotate(359deg)'
+    }
+], {
+    duration: 750,
+    iterations: Infinity
+});
+
+squareAnimation.pause();
+document.getElementById('square').style.backgroundColor = "#EF5350";
