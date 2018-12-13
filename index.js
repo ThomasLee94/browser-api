24784f735135abf5cd33706d5a6e6c6b211665d3
ThomasLee94 <thomaslee9474@gmail.com>
2018-12-13 02:36:41 -0800

    [updte] index.html

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const diagonalMove = [
     {
         transform: 'translate(0px, 0px)',
-        background: '#2196F3'
+        background: "#C0C0C0"
     },
     {
         offset: .5,
         transform: 'translate(115px, 115px)',
-        background: '#2196F3'
+        background: '#C0C0C0'
     },
     {
         transform: 'translate(230px, 230px)',
-        background: '#EF5350'
+        background: '#808080'
     }
 ];
 
@@ -29,22 +29,23 @@ const squareAnimation = square.animate([
 
 // * Pause feature
 squareAnimation.pause();
-document.getElementById('square').style.backgroundColor = "#EF5350";
+document.getElementById('square').style.backgroundColor = "#C0C0C0";
 
 document.getElementById('pause').addEventListener('click', function() {
   squareAnimation.pause();
-  document.getElementById('square').style.backgroundColor = "#EF5350";
+  document.getElementById('square').style.backgroundColor = "#C0C0C0";
 });
 
 // * Play feature
 document.getElementById('play').addEventListener('click', function() {
   squareAnimation.play();
-  document.getElementById('square').style.backgroundColor = "#2196F3";
+  document.getElementById('square').style.backgroundColor = '#808080';
 });
 
 // * Reverse feature
 document.getElementById('reverse').addEventListener('click', function() {
   squareAnimation.reverse();
+  document.getElementById('square').style.backgroundColor = '#808080';
 });
 
 
